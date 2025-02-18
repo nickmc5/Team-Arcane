a92c67d4726bbd90cca13776ba3c94927afa2be2
nickmc5 <nick.mcclure55@gmail.com>
2025-02-18 15:16:31 -0500

    Added better pause menu

--- Team Arcane/Assets/Scripts/MenuController.cs
+++ Team Arcane/Assets/Scripts/MenuController.cs
@@ -1,35 +1,36 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class MenuController : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject HUD;
     public GameObject inventory;
-    public int currentMenu = 0; // 0 = HUD, 1 = Pause, 2 = Inventory
+    public static int currentMenu = 0; // 0 = HUD, 1 = Pause, 2 = Inventory
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        switch(currentMenu)
+        inventory.SetActive(true);
+        switch (currentMenu)
         {
             case 0:
                 HUD.SetActive(true);
                 pauseMenu.SetActive(false);
-                inventory.SetActive(false);
+                //inventory.SetActive(false);
                 break;
             case 1:
                 HUD.SetActive(false);
                 pauseMenu.SetActive(true);
-                inventory.SetActive(false);
+                //inventory.SetActive(false);
                 break;
             case 2:
                 HUD.SetActive(false);
                 pauseMenu.SetActive(false);
-                inventory.SetActive(true);
+                //inventory.SetActive(true);
                 break;
         }
-            
-                
     }
 
     // Update is called once per frame
@@ -38,27 +39,49 @@ public class MenuController : MonoBehaviour
         // Menu switching system
         if (Input.GetKeyDown(KeyCode.Escape) && currentMenu == 0)
         {
-            pauseMenu.SetActive(true);
-            HUD.SetActive(false);
-            currentMenu = 1;
+            HUDToPause();
         }
         else if((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Q)) && currentMenu == 1)
         {
-            pauseMenu.SetActive(false);
-            HUD.SetActive(true);
-            currentMenu = 0;
+            PauseToHUD();
         }
         else if (Input.GetKeyDown(KeyCode.Q) && currentMenu == 0)
         {
-            inventory.SetActive(true);
-            HUD.SetActive(false);
-            currentMenu = 2;
+            HUDToInventory();
         }
         else if ((Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape)) && currentMenu == 2)
         {
-            inventory.SetActive(false);
-            HUD.SetActive(true);
-            currentMenu = 0;
+            StartCoroutine("InventoryToHUD");
         }
+    }
+
+    public void HUDToPause()
+    {
+        pauseMenu.SetActive(true);
+        HUD.SetActive(false);
+        currentMenu = 1;
+    }
+
+    public void PauseToHUD()
+    {
+        pauseMenu.SetActive(false);
+        HUD.SetActive(true);
+        currentMenu = 0;
+    }
+
+    public void HUDToInventory()
+    {
+        //inventory.SetActive(true);
+        HUD.SetActive(false);
+        currentMenu = 2;
+    }
+
+    IEnumerator InventoryToHUD()
+    {
+        //inventory.SetActive(false);
+        currentMenu = 0;
+        yield return new WaitForSeconds(1/4f); ;
+        HUD.SetActive(true);
+        
     }
 }
