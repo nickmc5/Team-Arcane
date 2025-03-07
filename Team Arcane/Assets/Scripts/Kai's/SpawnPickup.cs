2f4a2f066ff959c1bc28ddb02e834912fbd0dcaf
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-03-06 21:00:42 -0500

    Added comments to code

--- Team Arcane/Assets/Scripts/Kai's/SpawnPickup.cs
+++ Team Arcane/Assets/Scripts/Kai's/SpawnPickup.cs
@@ -5,6 +5,7 @@ public class SpawnPickup : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Despawns Object if already picked up in player inventory
         if (PersistantGameManager.masterInventory.ContainsKey(gameObject.name))
         {
             gameObject.SetActive(false);
