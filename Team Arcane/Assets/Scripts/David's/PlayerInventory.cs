f7784b29bdaaf3bded6e2a4f32360c7f5a1a0cbc
nickmc5 <nick.mcclure55@gmail.com>
2025-03-07 20:13:52 -0500

    Added ability to tab between items with icon in the HUD

--- Team Arcane/Assets/Scripts/David's/PlayerInventory.cs
+++ Team Arcane/Assets/Scripts/David's/PlayerInventory.cs
@@ -8,11 +8,33 @@ using TMPro;
 public class PlayerInventory : MonoBehaviour
 {
     public static Dictionary<string,Sprite> playerInv = new Dictionary<string, Sprite>();
+
+    private List<string> indexedInv = new List<string>();
+
     public GameObject inventoryUIItems;
+    public GameObject HUDItem;
+
+    private int currentItem = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && playerInv.Count != 0)
+        {
+            currentItem = (currentItem + 1) % playerInv.Count;
+            HUDItem.GetComponent<Image>().sprite = playerInv[indexedInv[currentItem]];
+            HUDItem.GetComponentInChildren<TextMeshProUGUI>().text = indexedInv[currentItem];
+        }
+    }
 
     public void AddItem(string n, Sprite i)
     {
+        if (playerInv.Count == 0)
+        {
+            UpdateHUDIcon(n, i);
+        }
+
         playerInv.Add(n, i);
+        indexedInv.Add(n);
         int j = 0;
         foreach (var item in playerInv)
         {
@@ -25,7 +47,27 @@ public class PlayerInventory : MonoBehaviour
 
     public void RemoveItem(string name)
     {
+        if (indexedInv[currentItem] == name)
+        {
+            currentItem = (currentItem + 1) % playerInv.Count;
+            UpdateHUDIcon(indexedInv[currentItem], playerInv[indexedInv[currentItem]]);
+        }
+
         playerInv.Remove(name);
+        indexedInv.Remove(name);
+
+        if (currentItem > playerInv.Count)
+        {
+            currentItem = 0;
+            UpdateHUDIcon(indexedInv[currentItem], playerInv[indexedInv[currentItem]]);
+        }
+        else if (playerInv.Count == 0)
+        {
+            currentItem = 0;
+            UpdateHUDIcon("No Items", Resources.Load<Sprite>("DiscIcon"));
+        }
+
+
         for (int i = 0; i < inventoryUIItems.transform.childCount; i++)
         {
             inventoryUIItems.transform.GetChild(i).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
@@ -40,5 +82,11 @@ public class PlayerInventory : MonoBehaviour
             inventoryUIItems.transform.GetChild(j).GetComponent<Image>().color = Color.white;
             j++;
         }
+    }
+
+    private void UpdateHUDIcon(string text, Sprite icon)
+    {
+        HUDItem.GetComponent<Image>().sprite = icon;
+        HUDItem.GetComponentInChildren<TextMeshProUGUI>().text = text;
     }
 }
