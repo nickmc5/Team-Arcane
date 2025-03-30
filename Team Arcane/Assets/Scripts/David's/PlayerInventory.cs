26cb0031baa2d2b0081aebcd454a04c1867d470f
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-03-30 10:20:34 -0400

    Removed comments and cleaned up code

--- Team Arcane/Assets/Scripts/David's/PlayerInventory.cs
+++ Team Arcane/Assets/Scripts/David's/PlayerInventory.cs
@@ -40,38 +40,25 @@ public class PlayerInventory : MonoBehaviour
         }
     }
 
+    // If not in first scene, load with player with previous inventory
     void Start()
     {
-        // If not in first scene, load with player with previous inventory
-        Debug.Log("Inventory Loading into new scene");
-        foreach (KeyValuePair<string, Sprite> pair in PersistantGameManager.masterInventory)
-        {
-            Debug.Log(pair.Key);
-            Debug.Log(pair.Value);
-        }
-        playerInv.Clear();
-        foreach (KeyValuePair<string, Sprite> pair in PersistantGameManager.masterInventory)
-        {
-            Debug.Log(pair.Key);
-            Debug.Log(pair.Value);
-        }
-        Debug.Log("Level Entry Point: " + PersistantGameManager.LevelEntryPoint);
+        
+        playerInv.Clear(); // Clear current local inventory
+        
+        // If Not coming from an initial level or inventory clearing level
         if (PersistantGameManager.LevelEntryPoint != -1)
         {
+            // Loop through current global inventroy and add to local inventory
             foreach (KeyValuePair<string, Sprite> pair in PersistantGameManager.masterInventory)
             {
-                // if (!PersistantGameManager.masterInventory.ContainsKey(pair.Key))
-                // {
                     AddItem(pair.Key, pair.Value);
-                    Debug.Log(pair.Key + ": was added");
-                // }
             }
         }
     }
 
     public void AddItem(string n, Sprite i)
     {
-        Debug.Log("HERE");
         if (playerInv.Count == 0)
         {
             UpdateHUDIcon(n, i);
@@ -94,24 +81,11 @@ public class PlayerInventory : MonoBehaviour
             inventoryUIItems.transform.GetChild(j).GetComponent<Image>().color = Color.white;
             j++;
         }
-
-        // foreach (KeyValuePair<string, Sprite> pair in playerInv)
-        // {
-        //     PersistantGameManager.masterInventory[pair.Key] = pair.Value;
-        // }
         
-        // If persistant game manager already contains key
+        // If persistant game manager doesn't already contain key, then add it
         if (!PersistantGameManager.masterInventory.ContainsKey(n))
         {
             PersistantGameManager.masterInventory.Add(n, i);
-        }
-        
-        // PersistantGameManager.masterInventory = playerInv;
-        Debug.Log("MASTER Inventory UPDATED");
-        foreach (KeyValuePair<string, Sprite> pair in PersistantGameManager.masterInventory)
-        {
-            Debug.Log(pair.Key);
-            Debug.Log(pair.Value);
         }
     }
 
