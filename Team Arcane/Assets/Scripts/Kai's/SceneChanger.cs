f8ac57da7b58e71f11502ec0b7fd2df2ab30f4f2
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-04-10 16:00:51 -0400

    Got first spawn point from room 1 to staircase working

--- Team Arcane/Assets/Scripts/Kai's/SceneChanger.cs
+++ Team Arcane/Assets/Scripts/Kai's/SceneChanger.cs
@@ -3,17 +3,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // Handles making any object being able to transport the player if touched. This Class both loads the scene and places the player in the correct orientation
+
+
 public class SceneChanger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public string SceneName;
     public int LevelEntryPoint = 1;
     public string SpawnPointName;
+    public static bool useSpawnPosition = false;
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (useSpawnPosition)
+        {
+            SetPlayerPosAndRot();
+            useSpawnPosition = false;
+        }
+    }
+
     void Start()
     {
+        Debug.Log("SETTING PLAYER");
         // Uncomment to spawn user in specific place
-        if (PersistantGameManager.LevelEntryPoint != -1) return;
+        if (PersistantGameManager.LevelEntryPoint == -1) return;
         // Initial Game Spawn and all new Scene automatically have a -1 entry point
+        Debug.Log("SETTING PLAYER");
         SetPlayerPosAndRot();
     }
 
@@ -39,12 +63,16 @@ public class SceneChanger : MonoBehaviour
        // Set player it designated spawn point here
        // NOTE: You can leverage persistant game manager to get check current loaded in scene
        string SpawnName = PersistantGameManager.SpawnPointName;
+       Debug.Log("SpawnName: " + SpawnName);
        GameObject SpawnPoint = GameObject.Find(SpawnName);
-
+       Debug.Log("SpawnPoint: " + SpawnPoint);
        if (SpawnPoint != null)
        {
-           GameObject Player = GameObject.Find(SpawnPoint.name);
+           GameObject Player = GameObject.Find("Player");
+           Debug.Log("Original Position: " + Player.transform.position);
+           SpawnPoint.transform.position = new Vector3(-7.57f, 6.129f, 11.09f);
            Player.transform.position = SpawnPoint.transform.position;
+           Debug.Log("Moving Player to " + Player.transform.position);
        }
     }
 }
