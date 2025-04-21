6fbb990820a3c3cd7e642221989c7d268ea0694f
nickmc5 <nick.mcclure55@gmail.com>
2025-04-21 19:36:43 -0400

    fixed charging room softlock

---
+++ Team Arcane/Assets/Scripts/Nick's/ChargingRoomQuest1.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ChargingRoomQuest1 : MonoBehaviour
+{
+    public BoxCollider doorLock;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PersistantGameManager.Instance.currentQuest != 0)
+        {
+            doorLock.isTrigger = true;
+        }
+    }
+}
